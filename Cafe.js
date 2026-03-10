60f24ae5f02e62ca39e0577a90be25aee7958a48
Zagirzaq <sajfullin2007@mail.ru>
2026-03-10 03:24:11 +0300

    fix main file

--- Cafe.js
+++ Cafe.js
@@ -1,7 +1,3 @@
-import { Tea } from "./Tea.js";
-import { Coffee } from "./Coffee.js";
-import { Milkshake } from "./Milkshake.js";
-
 export class Cafe {
     constructor(name, location) {
         this.name = name;
@@ -18,20 +14,3 @@ export class Cafe {
         drink.serve();
     }
 }
-
-
-const myCafe = new Cafe('Уютный уголок', 'ул. Программистов, д. 1');
-myCafe.getInfo();
-
-
-const americano = new Coffee('Американо', 'средний', 150, 20, 'Арабика', 'соевое');
-const greenTea = new Tea('Зелёный чай', 'большой', 120, 25, 'зелёный', 'жасмин');
-const berryShake = new Milkshake('Ягодный коктейль', 'маленький', 180, 6, 'малина', 'пломбир');
-
-myCafe.orderDrink(americano);
-myCafe.orderDrink(greenTea);
-myCafe.orderDrink(berryShake);
-
-console.log(americano.getInfo());
-americano.setTemperature(60);
-console.log(`Новая температура американо: ${americano.getTemperature()}°C`);
